c6630f85c8cda5cf4bde683766a27fa1e25d066c
sarasu2 <73024712+sarasu2@users.noreply.github.com>
2024-03-20 19:55:13 -0400

    use react Google Maps component

---
+++ access-maps/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+
+
+function Home() {
+    return (
+        <div>
+          <h1>This is the home page!</h1>
+          <h2>Navigage to /map to access the map</h2>
+        </div>
+      );
+}
+
+export default Home;
---
+++ access-maps/src/pages/Map.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+
+const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+const libraries = ['places'];
+const mapContainerStyle = {
+  width: '100vw',
+  height: '100vh',
+};
+
+const center = { lat: 34.6834, lng: -82.8374 };
+
+const Map = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
+  }
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={5}
+        center={center}
+      >
+        <Marker position={center}></Marker>
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Map;
